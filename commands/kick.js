bb874df650c1747d8c22e6770acdb2fff7c8fabf
TheGodKaua <51276245+TheGodKaua@users.noreply.github.com>
2019-06-05 20:07:29 -0300

    Create kick.js

---
+++ commands/kick.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js')
+
+module.exports.run = async (client, message, args) => {
+    if(!message.member.hasPermission('KICK_MEMBERS')) return message.reply("Você não tem **permissão** suficiente !")
+    let member = message.mentions.members.first()
+    if(!member)
+      return message.reply("Por favor mencione um usuário válido !")
+    if(!member.kickable)
+      return message.reply("Eu não posso kickar esse usuário, ele pode ter um cargo maior que o meu.")
+    let reason = args.slice(1).join(' ')
+    if(!reason) reason = "Nenhuma razão fornecida"
+    await member.kick(reason)
+      .catch(error => message.reply(`Desculpe ${message.author} não consegui kickar o membro devido o: ${error}`))
+
+      message.channel.send(`${message.author}`)
+
+      let pEmbed = new Discord.RichEmbed()
+          .setTitle(":hammer: Kick")
+          .addField("Membro Kickado:", `${member.user.tag}`)
+          .addField("Kickado por:", `${message.auhtor.tag}`)
+          .addField("Motivo:", `${reason}`)
+          .setFooter(`${message.auhtor.tag}`, message.author.displayAvatarURL)
+          .setColor("DARK_RED").setTimestamp()
+
+          message.channel.send(pEmbed)
+          
+}
+
+module.exports.help = {
+    name: "kick"
+}
