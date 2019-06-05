ea17ccae039fbbf4676a6c9d7c7973fdb3f3bd87
TheGodKaua <51276245+TheGodKaua@users.noreply.github.com>
2019-06-05 20:07:11 -0300

    Create ban.js

---
+++ commands/ban.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js')
+
+module.exports.run = async (client, message, args) => {
+    if(!message.member.hasPermission('BAN_MEMBERS')) return message.reply("Você não tem **permissão** suficiente !")
+    let member = message.mentions.members.first()
+    if(!member)
+      return message.reply("Por favor mencione um usuário válido !")
+    if(!member.bannable)
+      return message.reply("Eu não posso banir esse usuário, ele pode ter um cargo maior que o meu.")
+    let reason = args.slice(1).join(' ')
+    if(!reason) reason = "Nenhuma razão fornecida"
+    await member.ban(reason)
+      .catch(error => message.reply(`Desculpe ${message.author} não consegui banir o membro devido o: ${error}`))
+
+      message.channel.send(`${message.author}`)
+
+      let pEmbed = new Discord.RichEmbed()
+          .setTitle(":hammer: Ban")
+          .addField("Membro Banido:", `${member.user.tag}`)
+          .addField("Banido por:", `${message.auhtor.tag}`)
+          .addField("Motivo:", `${reason}`)
+          .setFooter(`${message.auhtor.tag}`, message.author.displayAvatarURL)
+          .setColor("DARK_RED").setTimestamp()
+
+          message.channel.send(pEmbed)
+          
+}
+
+module.exports.help = {
+    name: "ban"
+}
